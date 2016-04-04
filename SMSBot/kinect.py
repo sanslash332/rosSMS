0974387b941cfae6b62abed4c1e41d23cd570d8b
user <user@turtlebot.(none)>
2016-04-04 16:00:14 -0400

    agregue cosas al modulo kinect

--- SMSBot/kinect.py
+++ SMSBot/kinect.py
@@ -7,3 +7,48 @@ import numpy
 
 class KinectManager(object):
     """ clase que maneja todo lo referente a la cámara, el kinect e imágenes"""
+	def __init__(self):
+       		rospy.init_node('/SMSBot/Kinect', anonymous=True)
+		self.__depth_img = rospy.Subscriber('/camera/depth/image',Image ,self.depth_handler)
+ 		self.__rgb_img= rospy.Subscriber('/camera/rgb/image_color',Image,self.rgb_handler)
+		self.bridge = CvBridge()
+        	self.current_cv_depth_image = numpy.zeros((1,1,3))
+        	self.current_cv_rgb_image = numpy.zeros((1,1,3))
+
+
+	def depth_handler(self, data):
+			try:
+				D = numpy.asarray(self.bridge.imgmsg_to_cv(data,"32FC1"))[:,:540]
+				D = numpy.ma.masked_array(D, numpy.isnan(D))
+				self.current_cv_depth_image = D
+				#cv2.imshow("image_depth",D )
+				#cv2.waitKey(10)
+			except CvBridgeError, e:
+				print e
+
+	def rgb_handler(self, data):
+		try:
+		    self.current_cv_rgb_image = numpy.asarray(self.bridge.imgmsg_to_cv(data,"bgr8"))
+		    rospy.loginfo("imagen rgb recibida " + str(self.current_cv_rgb_image.shape))
+		    
+		    #cv2.imshow("image_rgb",self.current_cv_rgb_image )
+		    #cv2.waitKey(10)
+		    
+		except CvBridgeError, e:
+		    print e
+	def isWall(self)
+		R = self.current_cv_rgb_image
+		D = self.current_cv_depth_image
+		"""Aca yo veria si es que en un sector mas o menos grande, por ejemplo R[100:400, 150:350], es del mismo color	"""
+		banda = D2[100:350, :];
+		dist = banda1.mean(0);
+		if(numpy.mean(dist) < 0.7):
+			return 1			
+		return 0
+	def getAlignment(self)
+		D = self.current_cv_depth_image
+		"""Ve dos puntos de una pared y entrega la diferencia"""
+		banda = D2[100:350, :];
+		a1 = numpy.mean(banda[:,220:270])
+		a2 = numpy.mean(banda[:,370:420])
+		return a2-a1
--- SMSBot/movement.py
+++ SMSBot/movement.py
@@ -43,43 +43,77 @@ class MovementManager(object):
         
         while(dist< distance):
             x = self._position.x - posxInit
-            y= self._position.y-posyInit
+            y = self._position.y - posyInit
             dist = math.sqrt(math.pow(x, 2) + math.pow(y, 2))
             velx=vel
+
             if brakeHelp:
                 velx = self.kp * (distance - dist)
                 if velx > vel:
                     velx=vel
 
+	    if dist < 0.4: #Hace que la partida sea mas suave
+		velx = dist 
 
 
 
             data.linear.x = velx
             self.setVel(data)
             self.rate.sleep()
+
         data=Twist()
         self.setVel(data)
 
 
-
-
-
-
-
-    def moveRotate(self, angle, vel): 
+    def moveRotate(self, angle, vel, brakeHelp=False): 
         data = Twist()
         angInit = self._angle
         ang2 = 0
-        data.angular.z = vel
-        while(ang2 < angle):
-            self.setVel(data)
-            ang3 = self._angle
-            if ang3<0 and angInit>0:
-                ang3 = self._angle + 2*math.pi
-            ang2 = ang3 - angInit
-            self.rate.sleep()
-        data = Twist()		
-        self.setVel(data)
+	vela = vel
+	if(angle < 0):
+	    vela = -vel
+	if(angle > 0):
+		while(ang2 < angle):
+		    ang3 = self._angle
+		    if ang3<0 and angInit>0:
+		        ang3 = self._angle + 2*math.pi
+
+		    ang2 = ang3 - angInit
+
+		    if brakeHelp:
+		        vela = self.kp * (angle - ang2)
+		        if vela > vel:
+		            vela=vel
+
+		    if abs(ang2) < 0.35: #Hace que la partida sea mas suave
+			vela = 4 * ang2
+
+		    data.angular.z = vela
+		    self.setVel(data)
+		    self.rate.sleep()
+		data = Twist()		
+		self.setVel(data)
+	else:
+		while(ang2 > angle):
+		    ang3 = self._angle
+		    if ang3>0 and angInit<0:
+		        ang3 = self._angle - 2*math.pi
+
+		    ang2 = ang3 - angInit
+
+		    if brakeHelp:
+		        vela = self.kp * (angle - ang2)
+		        if vela > vel:
+		            vela=vel
+
+		    if abs(ang2) < 0.35: #Hace que la partida sea mas suave
+			vela = 4 * ang2
+
+		    data.angular.z = vela
+		    self.setVel(data)
+		    self.rate.sleep()
+		data = Twist()		
+		self.setVel(data)
 
 
     def getPosition(self):
@@ -88,7 +122,6 @@ class MovementManager(object):
     def getOrientation(self):
         return(self._orientation)
 
-    def getRadianAngle(self):
-        
+    def getRadianAngle(self):        
         return(self._angle)
 
