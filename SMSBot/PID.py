5d8d1016a8d0ad865cb20cb0bf9553e09ba96ce5
mirigotti <mirigotti@uc.cl>
2016-04-05 12:45:16 -0300

    Update PID.py

--- SMSBot/PID.py
+++ SMSBot/PID.py
@@ -3,22 +3,29 @@
 import rospy
 import roslib
 import numpy
+import time
 
 class PIDController(object):
     """ Clase para utilizar controladores del tipo PID"""
 
     def __init__(self, Kp, Ki, Kd):
         self.y = 0
-        self.u = 0
+        self.v = 0
         self.Kp = Kp
         self.Ki = Ki
         self.Kd = Kd
         self.er = (0, 0, 0)
+        self.Taux = time.time()
         
-    def calc_PID(e, dt):
+    def calc_PID(e):
+        end = time.time()
+        dT = end - self.Taux
+        
         self.er = (e, self.er[0], self.er[1])
-        du = Kp*(self.er[2]-self.er[1])+Ki*self.er[2]*dT+Kd*(self.er[2]-2*self.er[1]+self.er[0])/dt
-        self.u = self.u + du
+        dv = Kp*(self.er[2]-self.er[1])+Ki*self.er[2]*dT+Kd*(self.er[2]-2*self.er[1]+self.er[0])/dT
+        self.v = self.v + dv
         
-        return self.u
+        self.Taux = end
+        
+        return self.v
         
