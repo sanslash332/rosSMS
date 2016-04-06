23671ced0e08a50f52ee81c8927d8af0f5c48048
Sandl Okino <sanslash332@yahoo.es>
2016-04-06 14:37:12 -0300

    añadido módulo de sonido

--- SMSBot/robot.py
+++ SMSBot/robot.py
@@ -5,6 +5,7 @@ import roslib
 import numpy
 from SMSBot.movement import MovementManager
 from SMSBot.kinect import KinectManager
+from SMSBot.sound import SoundManager
 
 class Robot(object):
     """ Clase contenedora que representa al robot en sí mismo, y contiene todos los manejadores y partes que utilizaremos de él para manipularlo. Kinect, movimiento, sonido, etc"""
@@ -12,3 +13,5 @@ class Robot(object):
     def __init__(self):
         self.movement = MovementManager()
         self.kinect = KinectManager()
+        self.sound = SoundManager()
+
---
+++ SMSBot/sound.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+import rospy
+import roslib
+import numpy
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Point
+from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import Twist
+from SMSBot.PID import PIDController
+from SMSBot.kinect import KinectManager
+from sound_play.msg import SoundRequest
+
+
+class SoundManager(object):
+    """ Clase que controla y maneja la administración del sonido """
+
+    def __init__(self):
+        rospy.init_node('/SMSBot/SoundManager', anonymous=True)
+        self._soundPub = rospy.Publisher('robotsound', SoundRequest)
+        self.say("Sound started")
+
+
+    def playSound(self, snd):
+        s = SoundRequest()
+        s.sound = s.PLAY_FILE
+        s.command = s.PLAY_ONCE
+        s.arg = snd
+        self._soundPub.publish(s)
+
+
+
+    def say(self, msg):
+        s = SoundRequest()
+        s.sound = s.SAY
+        s.command= s.PLAY_ONCE
+        s.arg=msg
+        self._soundPub.publish(s)
+
+
+
+
+
+
+
+    
