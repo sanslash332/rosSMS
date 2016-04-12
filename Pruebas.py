b106fd075c750ae80d8e125b9b3bf54b9d12cd60
user <user@turtlebot.(none)>
2016-04-12 15:05:26 -0400

    actualizacion de datos

--- Pruebas.py
+++ Pruebas.py
@@ -1,10 +1,17 @@
 import rospy
+import roslib
 import numpy
+import math
+import cv2
+import time
+from SMSBot.robot import Robot
 
 def main():
-	D = numpy.random.rand(1,2,3)
-	print(D)
-	print(D[:,:,0:2])
+	tortuga = Robot()
+	while (tortuga.kinect.getDepthImage().shape == (1,1,3)):
+		rospy.loginfo("cargando kinect")
+	while True:
+		tortuga.kinect.showDepthImage()
 
 if __name__ == '__main__':
 	try:
