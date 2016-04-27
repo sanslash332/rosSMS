072972a35420dab8fb025e4108583544d4233909
Sandl Okino <sanslash332@yahoo.es>
2016-04-27 14:18:39 -0300

    subidos últimos cambios

--- Laberinto.py
+++ Laberinto.py
@@ -4,6 +4,8 @@ from busqueda.mapa import Mapa
 
 def main():
 	tortuga = Robot('e')
+	mapa = Mapa("mapa.txt")
+	
 	while (tortuga.kinect.getDepthImage().shape == (1,1,3)):
 		rospy.loginfo("cargando kinect")	
 	tortuga.sound.say("kinect ready")
@@ -12,10 +14,14 @@ def main():
 	#tortuga.correctDistance(0.5)	
 	
 	#tortuga.moveStraight(2, 0.3)
-	pasos = ['e','n','w','n','e']
+	tortuga.sound.playBuiltSound(3)
+	#pasos = ['e','n','w','n','e']
+	pasos= mapa.solveMap()
 	for p in pasos:
 		tortuga.moveMaze(p)
 	tortuga.sound.say("I got to the goal!")
+	tortuga.sound.playSound('/home/user/sounds/1up.wav')
+	
 	"""while(1):
 		tortuga.kinect.showDistances()
 		tortuga.movement.rate.sleep()"""
